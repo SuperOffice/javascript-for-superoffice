4fc04b9a1981f9acd086d74f306a7d3b4b0fcc2c
Eivind Fasting <eivind.fasting@amesto.no>
2023-09-25 15:54:03 +0200

    Added updatereadme-ts and scripts to compile the scripts so they can run

--- Scripts/update-readme.js
+++ Scripts/update-readme.js
@@ -1,38 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-
-const mappings = {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = require("fs");
+var path = require("path");
+var mappings = {
     '../Examples/Contact/CreateContactEntity.ts': 'CreateContactEntity.ts',
-    //'Examples/AnotherExample.ts': 'AnotherExample.ts',
+    // 'Examples/AnotherExample.ts': 'AnotherExample.ts',
     // ... add more mappings as needed
 };
-
 function updateCodeBlock(fileChanged) {
-    const blockIdentifier = mappings[fileChanged];
-    if (!blockIdentifier) return;
-
-    const codeFilePath = path.resolve(__dirname, fileChanged);
-    const readmePath = path.resolve(__dirname, '..', 'readme.md');
-
+    var blockIdentifier = mappings[fileChanged];
+    if (!blockIdentifier)
+        return;
+    var codeFilePath = path.resolve(__dirname, fileChanged);
+    var readmePath = path.resolve(__dirname, '..', 'readme.md');
     // Read the changing file
-    const codeContent = fs.readFileSync(codeFilePath, 'utf-8');
-    const newCodeBlock = `<!-- START:${blockIdentifier} -->\n\`\`\`typescript\n${codeContent}\n\`\`\`\n<!-- END:${blockIdentifier} -->`;
-
+    var codeContent = fs.readFileSync(codeFilePath, 'utf-8');
+    var newCodeBlock = "<!-- START:".concat(blockIdentifier, " -->\n```typescript\n").concat(codeContent, "\n```\n<!-- END:").concat(blockIdentifier, " -->");
     // Read the README
-    const readmeContent = fs.readFileSync(readmePath, 'utf-8');
-
+    var readmeContent = fs.readFileSync(readmePath, 'utf-8');
     // Use regex to replace the existing code block with the new one
-    const pattern = new RegExp(`<!-- START:${blockIdentifier} -->[\\s\\S]+?<!-- END:${blockIdentifier} -->`);
-    const updatedReadmeContent = readmeContent.replace(pattern, newCodeBlock);
-
+    var pattern = new RegExp("<!-- START:".concat(blockIdentifier, " -->[\\s\\S]+?<!-- END:").concat(blockIdentifier, " -->"));
+    var updatedReadmeContent = readmeContent.replace(pattern, newCodeBlock);
     // Write the updated content back to the README
     fs.writeFileSync(readmePath, updatedReadmeContent);
 }
-
 // Grab the filename from the command-line arguments
-const fileChanged = process.argv[2];
+var fileChanged = process.argv[2];
 if (fileChanged) {
     updateCodeBlock(fileChanged);
-} else {
+}
+else {
     console.log("Please provide the path to the file you changed.");
 }
---
+++ Scripts/update-readme.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const mappings: { [key: string]: string } = {
+    '../Examples/Contact/CreateContactEntity.ts': 'CreateContactEntity.ts',
+    // 'Examples/AnotherExample.ts': 'AnotherExample.ts',
+    // ... add more mappings as needed
+};
+
+function updateCodeBlock(fileChanged: string): void {
+    const blockIdentifier = mappings[fileChanged];
+    if (!blockIdentifier) return;
+
+    const codeFilePath = path.resolve(__dirname, fileChanged);
+    const readmePath = path.resolve(__dirname, '..', 'readme.md');
+
+    // Read the changing file
+    const codeContent: string = fs.readFileSync(codeFilePath, 'utf-8');
+    const newCodeBlock: string = `<!-- START:${blockIdentifier} -->\n\`\`\`typescript\n${codeContent}\n\`\`\`\n<!-- END:${blockIdentifier} -->`;
+
+    // Read the README
+    const readmeContent: string = fs.readFileSync(readmePath, 'utf-8');
+
+    // Use regex to replace the existing code block with the new one
+    const pattern = new RegExp(`<!-- START:${blockIdentifier} -->[\\s\\S]+?<!-- END:${blockIdentifier} -->`);
+    const updatedReadmeContent: string = readmeContent.replace(pattern, newCodeBlock);
+
+    // Write the updated content back to the README
+    fs.writeFileSync(readmePath, updatedReadmeContent);
+}
+
+// Grab the filename from the command-line arguments
+const fileChanged: string = process.argv[2];
+if (fileChanged) {
+    updateCodeBlock(fileChanged);
+} else {
+    console.log("Please provide the path to the file you changed.");
+}
