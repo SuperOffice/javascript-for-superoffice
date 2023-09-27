9c3a2de6e3129b05aeb729fc301056cb06b8c42e
Eivind Fasting <eivind.fasting@amesto.no>
2023-09-27 10:00:30 +0200

    edit readme.md

--- Examples/Contact/DeleteContactEntity.ts
+++ Examples/Contact/DeleteContactEntity.ts
@@ -8,4 +8,3 @@ const entityId: number = 2;
 const agent = new RTL.ContactAgent();
 await agent.deleteContactEntityAsync(entityId);
 context.result.body = 'EntityId ' + entityId.toString() + ' deleted';
-
